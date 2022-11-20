75e29c0e34e15eeb57542797a79fe0dded9877f1
won21yuk <won21yuk@gmail.com>
2022-11-20 21:38:25 +0900

    2022-11-20 dag update

--- dags/test_dag.py
+++ dags/test_dag.py
@@ -17,7 +17,7 @@ def test_callable():
     print(os.path.abspath(__file__))
     print(os.path.abspath(os.path.dirname(__file__)))
     print(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
-    print(os.path.join(os.environ['DAGS_FOLDER']))
+    print(os.path.join(os.environ['_GCS_BUCKET']))
 
 test = PythonOperator(task_id='test',
                       python_callable=test_callable,
